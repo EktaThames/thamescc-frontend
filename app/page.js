5e43063d6e7f481fdd3780c9d8cf64eb751d3fa2
Ekta Yadav <ekta.thames@gmail.com>
2025-05-15 19:21:01 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to ThamesCC!
+      </h1>
+    </main>
+    
+  );
+}
